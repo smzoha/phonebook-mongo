a350d77d8005153f8883c53897cd92aba272cca2
Shamah M Zoha <shamah1992@gmail.com>
2022-02-04 16:43:35 +0600

    Initial Commit
    Includes:
    * Setting up build script - adding required dependencies
    * Setting up Application class - the main class of the project
    * Added gitignore

---
+++ src/main/java/com/zedapps/phonebook/Application.java
@@ -0,0 +1,16 @@
+package com.zedapps.phonebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Shamah M Zoha
+ * @since 02-Feb-22
+ **/
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
