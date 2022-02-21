dc7341e3ea022401ff36017156da1499363999e6
Shamah M Zoha <shamah1992@gmail.com>
2022-02-22 00:01:51 +0600

    Added security config for resource & login
    The css, js, and webfonts static resources have been made available for
    all users through security config, so that they appear whenever the
    application pages are accessed.
    
    Configuration has also been done to account for the to-be-implemented
    login page.

---
+++ src/main/java/com/zedapps/phonebook/config/AppSecurityConfig.java
@@ -0,0 +1,47 @@
+package com.zedapps.phonebook.config;
+
+import com.zedapps.phonebook.service.AppUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+/**
+ * @author Shamah M Zoha
+ * @since 21-Feb-22
+ **/
+@Configuration
+@EnableWebSecurity
+public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private AppUserDetailsService appUserDetailsService;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/css/**", "/js/**", "/webfonts/**").permitAll()
+                .antMatchers("/login/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .defaultSuccessUrl("/", true);
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(appUserDetailsService)
+                .passwordEncoder(passwordEncoder());
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
